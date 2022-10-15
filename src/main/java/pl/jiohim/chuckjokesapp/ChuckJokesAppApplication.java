e0c79bd592508757e6e9892f460a89f5c9338263
Jiohim <bohdankrasin@gmail.com>
2022-10-15 17:10:03 +0200

    creating initial project

---
+++ src/main/java/pl/jiohim/chuckjokesapp/ChuckJokesAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jiohim.chuckjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jiohim/chuckjokesapp/ChuckJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jiohim.chuckjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
